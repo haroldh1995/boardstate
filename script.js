3808aa324c0c5b0c80eeb49eddd8c0a86753ed0f
haroldh1995 <haroldh1995@users.noreply.github.com>
2026-05-11 18:33:31 -0500

    fix: remove broken boardstate-upgrades imports

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import * as BoardStateUpgrades from "./src/boardstate-upgrades/boardstateUpgrades.js";
 import { DEFAULT_FACE_DOWN_LABEL, MULTIPLAYER_VIEW_MODES } from "./multiplayer/connectionTypes.js";
 import { createMultiplayerService } from "./multiplayer/multiplayerService.js";
 import { createPublicPlayerSnapshot, createViewerBoardState } from "./multiplayer/playerSnapshot.js";
